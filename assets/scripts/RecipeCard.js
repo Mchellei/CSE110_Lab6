cf2904110d104f170e89cccbe9e58baa39ff919b
Mchellei <97761873+Mchellei@users.noreply.github.com>
2023-05-14 14:27:11 -0700

    Update

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -136,7 +136,7 @@ class RecipeCard extends HTMLElement {
   <p class="organization">${data.organization}</p>
   <div class="rating">
     <span>${data.rating}</span>
-    <img src="https://github.com/Mchellei/CSE110_Lab6/blob/main/assets/images/icons/${data.rating}-star.svg">
+    <img src="CSE110-Lab6/assests/images/icons/${data.rating}-star.svg">
     <span>(${data.numRatings})</span>
   </div>
   <time>${data.lengthTime}</time>
